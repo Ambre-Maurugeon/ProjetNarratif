2cf0ffcae80d1ea52e88957da11b6c0502cd42e4
Stevens Tavares-Ramos <stevens.tavares@gmail.com>
2026-01-30 15:24:11 +0100

    [Sound Effect]

---
+++ Assets/Scripts/Pulse.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Pulse : MonoBehaviour
+{
+    [SerializeField]private AudioSource MusicSource;
+    [SerializeField]private float BPM;
+
+    private float beatInterval;
+    private float nextBeatInterval;
+
+    private AudioSource MusicToEnabled;
+
+    void Start()
+    {
+        InitMusic();
+    }
+
+    void Update()
+    {
+        NextBeat();
+    }
+
+
+    void InitMusic()
+    {
+        if (MusicSource == null)
+        {
+            Debug.Log("Select a sound in the field MusicSource of Heart Beats please");
+            return;
+        }
+          
+/*      MusicToEnabled = MusicSource.GetComponent<AudioSource>();
+        MusicToEnabled.enabled = true;*/
+        MusicSource.Play();
+        beatInterval = 60f / BPM;
+        nextBeatInterval = MusicSource.time + beatInterval;
+
+    }
+
+    void NextBeat() 
+    {
+        if (MusicSource.isPlaying && MusicSource.time >= nextBeatInterval) 
+        {
+            nextBeatInterval += beatInterval;
+            Debug.Log($"top : {MusicSource.time}");
+        }
+    }
+
+}
+
