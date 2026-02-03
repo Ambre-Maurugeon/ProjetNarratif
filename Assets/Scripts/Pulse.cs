896f7ebdb899bb6902815bceaa092b5262421653
Stevens Tavares-Ramos <stevens.tavares@gmail.com>
2026-02-03 13:37:14 +0100

    [PROTO] Rhytme music without end Event

--- Assets/Scripts/Pulse.cs
+++ Assets/Scripts/Pulse.cs
@@ -3,13 +3,23 @@ using System;
 
 public class Pulse : MonoBehaviour
 {
-    [SerializeField]private AudioSource MusicSource;
-    [SerializeField]private float BPM;
-    [SerializeField]private float speed;
-    [SerializeField]private float Tolerance;
+    [SerializeField] private AudioSource MusicSource;
+    [SerializeField] private float BPM;
+    [SerializeField] private float speed;
+    [SerializeField] private float Tolerance;
 
+    private double dspStartTime;
     private float beatInterval;
     private float nextBeatInterval;
+    private bool playing;
+    private int nbInterval = 0;
+    private int timingResult = 0;
+
+    public bool PerfectAchievement;
+    public bool GoodAchievement;
+    public bool MediumAchievement;
+    public bool BadAchievement;
+
 
     TouchScreen touchScreen;
 
@@ -25,6 +35,12 @@ public class Pulse : MonoBehaviour
 
     void Start()
     {
+        PerfectAchievement = false;
+        GoodAchievement = false;
+        MediumAchievement = false;
+        BadAchievement = false;
+        playing = false;
+
         InitMusic();
     }
 
@@ -42,41 +58,111 @@ public class Pulse : MonoBehaviour
             Debug.Log("Select a sound in the field MusicSource of Heart Beats please");
             return;
         }
-          
-/*      MusicToEnabled = MusicSource.GetComponent<AudioSource>();
-        MusicToEnabled.enabled = true;*/
-        MusicSource.Play();
+
         beatInterval = 60f / BPM;
-        nextBeatInterval = MusicSource.time + beatInterval;
-        /*MusicSource.pitch = beatInterval;*/
+        MusicSource.Play();
+
+        dspStartTime = AudioSettings.dspTime;
+        Debug.LogWarning(dspStartTime);
+        nextBeatInterval = (float)dspStartTime + beatInterval;
+
+        /*MusicSource.loop = true;*/
+        playing = true;
 
     }
 
-    void NextBeat() 
+    void NextBeat()
     {
-        if (MusicSource.isPlaying && MusicSource.time >= nextBeatInterval) 
+        if (!playing) return;
+
+        double bpmTimePlaying = dspStartTime;
+
+        if (bpmTimePlaying >= nextBeatInterval)
         {
             nextBeatInterval += beatInterval;
-            Debug.LogWarning($"top : {MusicSource.time}");
+            nbInterval++;
+            /*Debug.Log($"Count: {nbInterval}");*/
+            Debug.LogWarning($"nextBeatInterval : {nextBeatInterval}");
+        }
+
+        if (nbInterval >= 22) 
+        {
+            Result();
+            Stop();
         }
     }
 
     void BeatInterval()
     {
-        if (MusicSource.isPlaying && MusicSource.time >= nextBeatInterval - Tolerance || MusicSource.isPlaying && MusicSource.time <= nextBeatInterval + Tolerance)
+        if (!playing) return;
+
+        double tapTime = AudioSettings.dspTime - dspStartTime;
+        Debug.Log($"tapTime: {tapTime}");
+
+        // Beat le plus proche
+        double closestBeat = Math.Round(tapTime / beatInterval) * beatInterval;
+        Debug.Log($"closestBeat: {closestBeat}");
+        double delta = Math.Abs(tapTime - closestBeat);
+        Debug.Log($"delta: {delta}");
+
+        if (delta <= Tolerance)
         {
-            Debug.Log($"timing ok :{MusicSource.time}");
+            timingResult  += 1;
+            Debug.Log($"Timing OK");
         }
-        else 
+        else
         {
-            Debug.Log($"timing ko{MusicSource.time}");
+            timingResult -= 1;
+            Debug.Log($"Timing KO");
         }
     }
 
     void BPMSpeed(int speed)
     {
+        if (!playing) return;
         MusicSource.pitch = speed;
     }
 
-}
+        void Result()
+    {
 
+
+        if (timingResult < 0) 
+        {
+            timingResult = 1;
+        }
+
+        float result =  timingResult / nbInterval;
+
+        if (result == 1f)
+        {
+            Debug.Log("100%");
+            PerfectAchievement = true;
+        }
+        else if (result >= 0.8f && result < 1f)
+        {
+            Debug.Log("80% ~ 99%");
+            GoodAchievement = true;
+        }
+        else if (result >= 0.5f && result <= 0.7f)
+        {
+            Debug.Log("50% ~ 79%");
+            MediumAchievement = true;
+        }
+        else if (result < 0.4f)
+        {
+            Debug.Log("0% ~ 49%");
+            BadAchievement = true;
+        }
+    }
+
+    void Stop()
+    {
+        if (MusicSource.isPlaying)
+        {
+            MusicSource.Stop();
+            playing = false;
+        }
+    }
+
+}
